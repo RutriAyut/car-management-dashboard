61fe0804a11feb2336594ca8985192db6b126ad8
Rutri <rutri.ayutya@gmail.com>
2023-12-24 18:31:30 +0700

    chore(frontend): fix:router

--- frontendts/src/routes/Main/index.tsx
+++ frontendts/src/routes/Main/index.tsx
@@ -3,14 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import LayoutMain from '../../components/Layout/Main';
 import Home from './home';
 import Search from './Search';
-import Login from './login';
 const Main = () => {
 	return (
 		<LayoutMain>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/search" element={<Search />} />
-				<Route path="/login" element={<Login />} />
 			</Routes>
 		</LayoutMain>
 	);
