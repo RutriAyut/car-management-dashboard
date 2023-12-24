b4f6dbf11a2405b22f07c5ac11cca0688299400a
Rutri <rutri.ayutya@gmail.com>
2023-12-24 19:19:06 +0700

    chore(api): using dev

--- API/dist/index.js
+++ API/dist/index.js
@@ -1,50 +1,43 @@
-'use strict';
+"use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
-	return (mod && mod.__esModule) ? mod : { 'default': mod };
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, '__esModule', { value: true });
-const express_1 = __importDefault(require('express'));
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const knex_1 = __importDefault(require("knex"));
+const objection_1 = require("objection");
 const cors = require('cors');
 const YAML = require('yamljs');
 const swaggerUI = require('swagger-ui-express');
-// const carRouter = require('./src/routes/carRouter');
-// const authenticationRouter = require('./src/routes/authenticationRouter');
-// const userRouter = require('./src/routes/userRouter');
-// const typeRouter = require('./src/routes/typeRouter');
+const carRouter = require('./src/routes/carRouter');
+const authenticationRouter = require('./src/routes/authenticationRouter');
+const userRouter = require('./src/routes/userRouter');
+const typeRouter = require('./src/routes/typeRouter');
 const swaggerDocument = YAML.load('openAPI.yaml');
-// const knexInstance = knex({
-// 	client: 'postgresql',
-// 	connection: {
-// 		database: 'db_car_rental',
-// 		user: 'rutri',
-// 		password: 'rutri',
-// 		// filename: "./dev.sqlite3"
-// 	},
-// });
-// Model.knex(knexInstance);
+const knexInstance = (0, knex_1.default)({
+    client: 'postgresql',
+    connection: {
+        database: 'db_car_rental',
+        user: 'rutri',
+        password: 'rutri',
+        // filename: "./dev.sqlite3"
+    },
+});
+objection_1.Model.knex(knexInstance);
 const app = (0, express_1.default)();
-// const PORT: number = 8000;
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
 app.use(cors());
 // set routing
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-// app.use('/cars', carRouter);
-// app.use('/user', authenticationRouter); // kebutuhan login dan register
-// app.use('/manage', userRouter); // segala sesuatu yang berhubungan dengan users
-// app.use('/type', typeRouter);
+app.use('/cars', carRouter);
+app.use('/user', authenticationRouter); // kebutuhan login dan register
+app.use('/manage', userRouter); // segala sesuatu yang berhubungan dengan users
+app.use('/type', typeRouter);
 app.get('/', (_, res) => {
-	res.send('Express + TypeScript Server ++++++');
+    res.send('Express + TypeScript Server ++++++');
 });
-//listen port
-// const server = app.listen(PORT, () => {
-// 	console.log(`Express nyala di http://localhost:${PORT}`);
-// });
-// import App from './app';
-// const app = new App();
-// const server = app.start();
-// export default app;
 const PORT = 8000;
 app.listen(PORT, () => {
-	console.log(`Express nyala di http://localhost:${PORT}`);
+    console.log(`Express nyala di http://localhost:${PORT}`);
 });
--- API/index.ts
+++ API/index.ts
@@ -1,6 +1,6 @@
 import express, { Express } from 'express';
-// import knex from 'knex';
-// import { Model } from 'objection';
+import knex from 'knex';
+import { Model } from 'objection';
 import { Response } from 'express';
 
 
@@ -8,26 +8,24 @@ const cors = require('cors');
 const YAML = require('yamljs');
 const swaggerUI = require('swagger-ui-express');
 
-// const carRouter = require('./src/routes/carRouter');
-// const authenticationRouter = require('./src/routes/authenticationRouter');
-// const userRouter = require('./src/routes/userRouter');
-// const typeRouter = require('./src/routes/typeRouter');
+const carRouter = require('./src/routes/carRouter');
+const authenticationRouter = require('./src/routes/authenticationRouter');
+const userRouter = require('./src/routes/userRouter');
+const typeRouter = require('./src/routes/typeRouter');
 
 const swaggerDocument = YAML.load('openAPI.yaml');
-// const knexInstance = knex({
-// 	client: 'postgresql',
-// 	connection: {
-// 		database: 'db_car_rental',
-// 		user: 'rutri',
-// 		password: 'rutri',
-// 		// filename: "./dev.sqlite3"
-// 	},
-// });
+const knexInstance = knex({
+	client: 'postgresql',
+	connection: {
+		database: 'db_car_rental',
+		user: 'rutri',
+		password: 'rutri',
+		// filename: "./dev.sqlite3"
+	},
+});
 
-// Model.knex(knexInstance);
-
+Model.knex(knexInstance);
 const app: Express = express();
-// const PORT: number = 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -39,25 +37,14 @@ app.use(
 	swaggerUI.serve,
 	swaggerUI.setup(swaggerDocument)
 );
-// app.use('/cars', carRouter);
-// app.use('/user', authenticationRouter); // kebutuhan login dan register
-// app.use('/manage', userRouter); // segala sesuatu yang berhubungan dengan users
-// app.use('/type', typeRouter);
+app.use('/cars', carRouter);
+app.use('/user', authenticationRouter); // kebutuhan login dan register
+app.use('/manage', userRouter); // segala sesuatu yang berhubungan dengan users
+app.use('/type', typeRouter);
 
 app.get('/', (_, res: Response) => {
 	res.send('Express + TypeScript Server ++++++');
 });
-
-//listen port
-// const server = app.listen(PORT, () => {
-// 	console.log(`Express nyala di http://localhost:${PORT}`);
-// });
-
-// import App from './app';
-// const app = new App();
-// const server = app.start();
-
-// export default app;
 
 const PORT= 8000;
 app.listen(PORT, () => {
