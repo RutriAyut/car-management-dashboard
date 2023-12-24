107deffea860f428e39b9ff631767ea105c413a0
Rutri <rutri.ayutya@gmail.com>
2023-12-24 18:23:38 +0700

    chore(frontend): fix:router

--- frontendts/src/routes/index.tsx
+++ frontendts/src/routes/index.tsx
@@ -9,7 +9,7 @@ const RouterApp = () => {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/login" element={<Login />} />
-				<Route path="/main/*" element={<Main />} />
+				<Route path="/*" element={<Main />} />
 				<Route
 					path="/admin/*"
 					element={
