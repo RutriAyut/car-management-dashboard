972c0b48867fb7b987550a65a33dc3377bd721f7
Rutri <rutri.ayutya@gmail.com>
2023-12-24 21:07:39 +0700

    core(api) fix: database

--- API/knexfile.ts
+++ API/knexfile.ts
@@ -1,49 +1,7 @@
-import type { Knex } from 'knex';
-
-// Update with your config settings.
-
-const config: { [key: string]: Knex.Config } = {
-	development: {
-		client: 'postgresql',
-		connection: {
-			database: 'db_car_rental',
-			user: 'rutri',
-			password: 'rutri',
-			// filename: "./dev.sqlite3"
-		},
-	},
-
-	staging: {
-		client: 'postgresql',
-		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password',
-		},
-		pool: {
-			min: 2,
-			max: 10,
-		},
-		migrations: {
-			tableName: 'knex_migrations',
-		},
-	},
-
-	production: {
-		client: 'postgresql',
-		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password',
-		},
-		pool: {
-			min: 2,
-			max: 10,
-		},
-		migrations: {
-			tableName: 'knex_migrations',
-		},
-	},
+require('dotenv').config();
+const pg = require('pg');
+pg.defaults.ssl = true;
+module.exports = {
+	client: 'pg',
+	connection: process.env.DATABASE_URL
 };
-
-module.exports = config;
